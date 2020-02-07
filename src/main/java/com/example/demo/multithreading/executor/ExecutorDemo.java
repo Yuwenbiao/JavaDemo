6f2ebd35236255d27e6fdd71179ba308e78699fd
余文彪 <yuwb@corp.21cn.com>
2020-02-07 20:00:07 +0800

    - 自定义线程名

--- src/main/java/com/example/demo/multithreading/executor/ExecutorDemo.java
+++ src/main/java/com/example/demo/multithreading/executor/ExecutorDemo.java
@@ -1,9 +1,6 @@
 package com.example.demo.multithreading.executor;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * 线程池框架executor示例
@@ -13,10 +10,21 @@ import java.util.concurrent.TimeUnit;
  */
 public class ExecutorDemo {
     /**
-     * 自定义线程名
+     * 自定义线程名方式1
      */
     private static ThreadFactory threadFactory = r -> new Thread(r, "t_pl_pool_" + r.hashCode());
 
     public static ThreadPoolExecutor executePool = new ThreadPoolExecutor(50, 200, 60, TimeUnit.SECONDS,
             new ArrayBlockingQueue<>(5000), threadFactory, new ThreadPoolExecutor.DiscardOldestPolicy());
+
+    /**
+     * 自定义线程名方式2：在线程方法中设置名称
+     */
+    static class TestCallable implements Callable<String> {
+        @Override
+        public String call() throws Exception {
+            Thread.currentThread().setName("test");
+            return "test call";
+        }
+    }
 }
